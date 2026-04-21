17261ac98051388d9380bf29104923178a42870e
OmarKh006 <khairyomar006@gmail.com>
2026-04-21 06:26:23 +0200

    switching between home and add notes pages

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+const navigationButtons = document.querySelectorAll(".header__btn");
+const homePage = document.querySelector("#home-page");
+const addNotePage = document.querySelector("#add-note-page");
+
+navigationButtons.forEach((button) => {
+  button.addEventListener("click", (clicked) => {
+    navigationButtons.forEach((b) => {
+      b.removeAttribute("aria-current");
+    });
+    clicked.currentTarget.setAttribute("aria-current", "page");
+    if (clicked.currentTarget.dataset.page === "home") setViewedPage("home");
+    else if (clicked.currentTarget.dataset.page === "note")
+      setViewedPage("note");
+  });
+});
+
+const setViewedPage = (page) => {
+  if (page === "home") {
+    homePage.hidden = false;
+    addNotePage.hidden = true;
+  } else if (page === "note") {
+    homePage.hidden = true;
+    addNotePage.hidden = false;
+  }
+  return;
+};
